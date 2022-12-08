85192d96000a46c556e5d7c7b509a42988247558
Angel Sepulveda <angelsepulvedasep@gmail.com>
2022-12-08 18:04:03 -0300

    implementacion de layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import {DataGrid} from "@mui/x-data-grid";
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
     return (
-        <></>
+        <>
+            <DataGrid
+                disableColumnSelector
+                disableSelectionOnClick
+                autoHeight
+
+            />
+        </>
     )
 }
 
